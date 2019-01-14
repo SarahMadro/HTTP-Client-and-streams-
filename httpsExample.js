632ce30e009b3585079b6236537d5dd01cb38b95
Sarah <sjmadro1@gmail.com>
2019-01-14 03:23:35 +0000

    response to callback

--- httpsExample.js
+++ httpsExample.js
@@ -1,2 +1,18 @@
 var https = require('https');
 console.log('I did it!');
+
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+var callback = function(response) {
+  console.log('In response handler callback!');
+  console.log('Response: ', response);
+}
+
+console.log('Im about to make the request');
+
+https.request(options, callback).end();
+
+console.log('Ive made the request');
